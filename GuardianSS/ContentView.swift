043a9053724f05fef76f87c88ec764e0ff56f60e
edefrutos <16051333+edfrutos@users.noreply.github.com>
2026-08-01 19:33:05 +0200

    Initial Commit

---
+++ GuardianSS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuardianSS
+//
+//  Created by Eugenio de Frutos Sanchez on 01/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuardianSS/GuardianSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardianSSApp.swift
+//  GuardianSS
+//
+//  Created by Eugenio de Frutos Sanchez on 01/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct GuardianSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
